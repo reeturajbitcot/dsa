08be0b49f13d48c8e3b4d1a80c63cdaa5ad79005
Reetu raj <reeturaj@bitcot.com>
2025-08-19 11:14:30 +0530

    aug-18

--- sorting.js
+++ sorting.js
@@ -13,14 +13,13 @@
 // bubble sort
 
 const bubbleSort = (array) => {
-    let itration = array.length
-    for (let i = 0; i < itration ; i++){
-         for (let j = 0 ; j < array.length ; j++){
-             if(array[j - 1] > array[j]){
-                 [array[j-1], array[j]] = [array[j], array[j - 1]]
+    for (let i = 0; i < array.length ; i++){
+         for (let j = 0 ; j < array.length - i - 1 ; j++){
+             if(array[j] > array[j + 1]){
+                 [array[j], array[j + 1]] = [array[j + 1], array[j]]
              }
          }
-        itration--
     }
     return array
 }
+
