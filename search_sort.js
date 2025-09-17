f831e3bcad504e40ef7cd22f4ea88cc286e2b65c
Reetu raj <reeturaj@bitcot.com>
2025-09-17 12:47:42 +0530

    sep-17

--- search_sort.js
+++ search_sort.js
@@ -51,3 +51,41 @@ const minimumRotatedSortedArray = (arr) => {
     }
     return arr[left]
 }
+
+const serarchInRotatedArray = (nums, target) => {
+    let left = 0;
+    let right = nums.length -1 ;
+    let minimum_element = 0
+    while (left < right){
+        let mid = left + Math.floor((right - left)/2)
+        if(nums[right] < nums[mid]){
+            left = mid + 1    
+        }else {
+            right = mid
+        }
+    }
+    minimum_element = left;
+
+    if(minimum_element === 0){
+        left = 0 ;
+        right = nums.length - 1;
+    }else if(target >= nums[0] && target <= nums[minimum_element - 1]){
+        left = 0 ;
+        rigth = minimum_element - 1
+    }else {
+        left = minimum_element
+        right = nums.length - 1
+    }
+
+    while (left <= right){
+        let mid = left + Math.floor((right - left) / 2)
+        if(nums[mid] ===  target){
+            return mid 
+        }else if (nums[mid] > target){
+            right = mid-1
+        }else {
+            left = mid + 1
+        }
+    }
+   return -1
+}
